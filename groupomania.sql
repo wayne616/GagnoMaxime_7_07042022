f7bff374608d726a60c6282a128a5c9aee140663
wayne616 <81424208+wayne616@users.noreply.github.com>
2022-07-07 13:09:07 +0200

    update 16

--- groupomania.sql
+++ groupomania.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : mer. 01 juin 2022 à 21:21
+-- Généré le : jeu. 07 juil. 2022 à 13:08
 -- Version du serveur : 10.4.22-MariaDB
 -- Version de PHP : 8.1.2
 
@@ -32,17 +32,19 @@ CREATE TABLE `message_send` (
   `text` varchar(500) DEFAULT NULL,
   `img` varchar(500) DEFAULT NULL,
   `date` timestamp NOT NULL DEFAULT current_timestamp(),
-  `user_id` int(11) NOT NULL
+  `user_id` int(11) NOT NULL,
+  `admin` int(1) NOT NULL DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Déchargement des données de la table `message_send`
 --
 
-INSERT INTO `message_send` (`Id`, `text`, `img`, `date`, `user_id`) VALUES
-(291, 'Hello World !!????????', NULL, '2022-06-01 15:18:03', 27),
-(292, 'Hi !!', NULL, '2022-06-01 15:18:51', 36),
-(293, 'test 3', NULL, '2022-06-01 16:57:27', 37);
+INSERT INTO `message_send` (`Id`, `text`, `img`, `date`, `user_id`, `admin`) VALUES
+(314, 'Hello World', NULL, '2022-07-07 10:18:33', 42, 1),
+(315, 'Hello World maxime admin', NULL, '2022-07-07 10:18:53', 39, 1),
+(316, 'Hi !!', NULL, '2022-07-07 10:19:05', 40, 1),
+(317, 'test 3', NULL, '2022-07-07 10:19:18', 41, 1);
 
 -- --------------------------------------------------------
 
@@ -64,10 +66,10 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`Id`, `Nom`, `Prenom`, `Password`, `Email`, `admin`) VALUES
-(27, 'Gagno', 'Maxime', '$2b$10$Pp.M77Q.WGjPNQBDOmePEuLyo6ENtXAalNb7FcZaxr13Z8B94E2zu', 'test@test.fr', 1),
-(36, 'AZERTY', 'YTREZA', '$2b$10$p/6zskCIhjkBUxI/tbkiRuKgKV48WsJ6AidjDGyV/OSxJEwAtXpsW', 'test2@test2.fr', 0),
-(37, 'QSDFGH', 'HGFDSQ', '$2b$10$qqhZbGZS/sGtTmJioowcR.IljyDJEquTVcMHVmVhNHqbGd7VTZES.', 'test3@test3.fr', 0),
-(38, 'WXCVBN', 'NBVCXW', '$2b$10$PBDpsOhUoT2vS3g1YFbwz.gHgnmqq/wKuKCWcBk0C.dSntMoeUrAq', 'test4@test4.fr', 0);
+(39, 'Maxime', 'Gagno', '$2b$10$d65I72/nVo7zRMXTpw39hO6AMavp2f0mJZT7Wixs6gM2QgmqA86Qu', 'test@test.fr', 1),
+(40, 'aaaa', 'aaaa', '$2b$10$xnfLUaYyranRclJkif4CK.U3rLMf4WzGqk/WUx4967XVU1/.ZIJgm', 'test2@test2.fr', 0),
+(41, 'bbbbb', 'bbbbb', '$2b$10$cD5HLrZqT68B5hY3bsfg4urCCOvA431Ba/DUOGwgspep62JVubGs6', 'test3@test3.fr', 0),
+(42, 'cccc', 'cccc', '$2b$10$gX7Z.hYBtFk5g3wmj2jk2.arWK0qbTdpY8bKxkNeM4f0xL0aLqtEm', 'test4@test4.fr', 0);
 
 --
 -- Index pour les tables déchargées
@@ -85,7 +87,8 @@ ALTER TABLE `message_send`
 --
 ALTER TABLE `user`
   ADD PRIMARY KEY (`Id`),
-  ADD UNIQUE KEY `Email` (`Email`);
+  ADD UNIQUE KEY `Email` (`Email`),
+  ADD KEY `admin` (`admin`);
 
 --
 -- AUTO_INCREMENT pour les tables déchargées
@@ -95,13 +98,23 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT pour la table `message_send`
 --
 ALTER TABLE `message_send`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=295;
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=318;
 
 --
 -- AUTO_INCREMENT pour la table `user`
 --
 ALTER TABLE `user`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `message_send`
+--
+ALTER TABLE `message_send`
+  ADD CONSTRAINT `user_id_send` FOREIGN KEY (`user_id`) REFERENCES `user` (`Id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
